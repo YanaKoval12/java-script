8813f30f117a70111881790a5d9fbeecb70c1199
Yana Koval <joannekoval739@gmail.com>
2022-06-30 14:16:14 +0300

    js

---
+++ lesson-6-task-4/index.js
@@ -0,0 +1,22 @@
+const increaseEvenEl = (arr, delta) => {
+    if (!Array.isArray(arr)) {
+    return null
+  }
+  
+  const result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      result.push(arr[i] + delta);
+    } else {
+      result.push(arr[i]);
+    }
+  }
+
+  return result;
+};
+
+// examples
+increaseEvenEl([2, 5, 6, 8, 11, 9, 4], 20); // ===> [22, 5, 26, 28, 11, 9, 24]
+increaseEvenEl([7, 11, 1], 10); // ===> [7, 11, 1]
+increaseEvenEl([], 120); // ===> []
