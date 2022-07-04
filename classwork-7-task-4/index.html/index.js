ea5260dd567ca4223c1ec3d45aa2a176b7b98570
Yana Koval <joannekoval739@gmail.com>
2022-07-04 12:23:22 +0300

    js

---
+++ classwork-7-task-4/index.html/index.js
@@ -0,0 +1,18 @@
+const getMessagesForBestStudents = (allStudentsList, studentsForRetake) => {
+    const passdeStudents = allStudentsList
+    .filter(name => !studentsForRetake.includes(name));
+  
+    const messages = passdeStudents
+    .map(name => 'Good job, ' +  name);
+  
+    return messages;
+  };
+  
+  // examples
+  const allStudTest1 = ['Den', 'John', 'Ann', 'Mike'];
+  const retakeStudTest1 = ['John', 'Mike'];
+  console.log(getMessagesForBestStudents(allStudTest1, retakeStudTest1)); // ===> ['Good job, Den', 'Good job, Ann']
+  
+  const allStudTest2 = ['Den', 'John', 'Ann', 'Mike'];
+  const retakeStudTest2 = ['Den', 'John', 'Ann', 'Mike'];
+  console.log(getMessagesForBestStudents(allStudTest2, retakeStudTest2)); // ===> []
